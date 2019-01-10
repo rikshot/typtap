fc5d15a2e6c6d14a2e45d53b0132b71a7c06eeda
Ville Orkas <villeo@unity3d.com>
2019-01-10 11:57:34 +0200

    Increase coverage

--- test/test_test.ts
+++ test/test_test.ts
@@ -8,6 +8,34 @@ test('false', (c) => {
     c.assert(false, false, 'check false');
 });
 
+test('empty array', (c) => {
+    c.assert([], [], 'check empty array');
+});
+
+test('array', (c) => {
+    c.assert([true, false, null, [], {}, undefined], [true, false, null, [], {}, undefined], 'check array');
+    c.assert([], [true], 'check array length');
+    c.assert([true], [false], 'check not equal array');
+    c.assert({}, [], 'check not array');
+});
+
+test('date', (c) => {
+    c.assert(new Date(), new Date(), 'check date');
+    c.assert(new Date(), {}, 'check inequal date');
+});
+
+test('regexp', (c) => {
+    c.assert(new RegExp(''), new RegExp(''), 'check regexp');
+    c.assert(new RegExp(''), {}, 'check not regexp');
+});
+
+test('object', (c) => {
+    c.assert({value: true}, {value: true}, 'check object');
+    c.assert({value: true}, {foo: true, bar: false}, 'check object keys');
+    c.assert({value: true}, {foo: false}, 'check object keys');
+    c.assert({value: true}, {value: false}, 'check inequal object');
+});
+
 test('throw error', (c) => {
     throw new Error('error');
 });
