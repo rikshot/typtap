412949ea4552aa06cd2bc0438dc91ba71757fd46
Ville Orkas <villeo@unity3d.com>
2019-01-11 16:41:35 +0200

    Execute tests serially & allow returning Promises

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,7 @@
 import { ITyptapReporter, Tap } from 'tap';
 import { equal } from './assert';
 
-export type ITestFunction = (description: string, runner: (context: ITestContext) => void) => void;
+export type ITestFunction = (description: string, runner: (context: ITestContext) => void | Promise<void>) => void;
 
 export interface ITestContext {
     test: ITestFunction;
@@ -26,12 +26,12 @@ export class Typtap {
     private readonly reporter: ITyptapReporter;
     private readonly context: ITestContext;
 
-    private readonly tests: Array<Promise<void>> = [];
+    private readonly tests: Array<() => Promise<void>> = [];
 
     private constructor(reporter: ITyptapReporter) {
         this.reporter = reporter;
         this.context = {
-            assert: (actual: unknown, expected: unknown, message?: string) => {
+            assert: (actual: any, expected: any, message?: string) => {
                 const passed = equal(actual, expected);
                 if (passed) {
                     ++this.passed;
@@ -48,29 +48,29 @@ export class Typtap {
         };
     }
 
-    public test(description: string, runner: (context: ITestContext) => void) {
+    public test(description: string, runner: (context: ITestContext) => void | Promise<void>) {
         ++this.counter;
-        this.tests.push(new Promise((resolve) => {
+        this.tests.push(async () => {
             this.reporter.label(description);
             try {
-                runner(this.context);
-                resolve();
+                await runner(this.context);
             } catch (error) {
                 this.reporter.error(error);
-                resolve();
             }
-        }));
+        });
     }
 
     public async run() {
         this.reporter.start();
-        await Promise.all(this.tests);
+        for (const runner of this.tests) {
+            await runner();
+        }
         this.reporter.end(this.passed, this.failed);
         return {passed: this.passed, failed: this.failed};
     }
 
 }
 
-export const test = (description: string, runner: (context: ITestContext) => void) => {
+export const test: ITestFunction = (description: string, runner: (context: ITestContext) => void | Promise<void>) => {
     Typtap.Default.test(description, runner);
 };
